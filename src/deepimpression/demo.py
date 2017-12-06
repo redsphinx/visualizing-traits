c9d025dc9fc981b70a4cac9634abcd75709ac3d5
redsphinx <gabi.ras@hotmail.com>
2017-12-06 12:18:40 +0100

    copy code from deepimpression

---
+++ src/deepimpression/demo.py
@@ -0,0 +1,39 @@
+from util import load_model, predict_trait, find_video, track_prediction, get_accuracy
+import pickle as pkl
+import os
+
+
+def main():
+    model = load_model()
+
+    # get ground truth from the pkl file
+	# ----------------------------------------------------------------------------
+    pkl_path = 'chalearn_fi_17_compressed/annotation_test.pkl'
+	# ----------------------------------------------------------------------------
+
+    f = open(pkl_path, 'r')
+    annotation_test = pkl.load(f)
+    # ['extraversion', 'neuroticism', 'agreeableness', 'conscientiousness', 'interview', 'openness']
+    annotation_test_keys = annotation_test.keys()
+    all_video_names = annotation_test[annotation_test_keys[0]].keys()
+
+    for ind in range(2000):
+        print('ind: ', ind)
+        video_id = all_video_names[ind]
+        target_labels = [annotation_test['extraversion'][video_id],
+                         annotation_test['agreeableness'][video_id],
+                         annotation_test['conscientiousness'][video_id],
+                         annotation_test['neuroticism'][video_id],
+                         annotation_test['openness'][video_id]]
+        video = find_video(video_id)
+        y = predict_trait(video, model)
+        print(video_id)
+        print('ValueExtraversion, ValueAgreeableness, ValueConscientiousness, ValueNeurotisicm, ValueOpenness')
+        print(y)
+        print(target_labels)
+        track_prediction(video_id, y, target_labels)
+	
+	# calculate and print mean performance
+	get_accuracy('performance_chalearn.txt')
+
+main()
---
+++ src/deepimpression/util.py
@@ -0,0 +1,104 @@
+import audiovisual_stream
+import chainer.serializers
+import librosa
+import numpy as np
+import skvideo.io
+import os
+
+
+def load_audio(data):
+    return librosa.load(data, 16000)[0][None, None, None, :]
+
+
+def load_model():
+    print('loading model')
+    model = audiovisual_stream.ResNet18().to_gpu(device='0')
+    # model = audiovisual_stream.ResNet18()
+
+    # maybe here?
+    chainer.serializers.load_npz('./model', model)
+
+    return model
+
+
+def load_video(data):
+    print('loading data')
+    video_capture = skvideo.io.vread(data)
+
+    # frames = 50
+    # video_capture = video_capture[:frames]
+
+    video_shape = np.shape(video_capture)
+    print('video shape: ', video_shape)
+    frames = video_shape[0]
+    video_capture = np.reshape(video_capture, (frames, video_shape[-1], video_shape[1], video_shape[2]), 'float32')
+
+    return np.array(video_capture, 'float32')
+
+
+def predict_trait(data, model):
+    print('predicting trait')
+    x = [load_audio(data), load_video(data)]
+    print('now really predicting. this will take a while.')
+    with chainer.using_config('train', False):
+        return model(x)
+    # return model(x)
+
+
+def find_video(video_id):
+    print('finding video: ', video_id)
+	# ----------------------------------------------------------------------------
+    base_path_1 = 'chalearn_fi_17_compressed/test-1'
+    base_path_2 = 'chalearn_fi_17_compressed/test-2'
+	# ----------------------------------------------------------------------------
+    video = None
+
+    not_found = True
+    while not_found:
+        for item in [base_path_1, base_path_2]:
+            all_dirs = os.listdir(item)
+            for d in all_dirs:
+                all_vids = os.listdir(os.path.join(item, d))
+                for vid in all_vids:
+                    if vid == video_id:
+                        video = os.path.join(item, d, vid)
+                        not_found = False
+    return video
+
+
+def track_prediction(video_id, prediction, target):
+    print('tracking prediction')
+    output_path = 'performance_chalearn.txt'
+    if not os.path.exists(output_path):
+        # create file
+        output_file = open(output_path, 'w')
+        output_file.close()
+
+    with open(output_path, 'a') as my_file:
+        my_file.write('%s,\n%s,\n%s,\n%s\n' % (video_id, str(prediction), str(target), str(target - prediction)))
+
+
+def get_accuracy(output_path):
+    result_per_trait = np.zeros((2000, 5), dtype=float)
+    result_total = np.zeros(2000, dtype=float)
+
+    with open(output_path, 'r') as my_file:
+        all_lines = my_file.readlines()
+
+    for ind in range(2000):
+        diff = all_lines[ind * 4 + 3]
+        diff = diff.split('\n')[0].split('[')[-1].split(']')[0]
+        diff = np.fromstring(diff, sep=' ', dtype=float)
+        diff = np.abs(diff)
+        print(ind * 4 + 3, diff, type(diff), np.shape(diff))
+        result_per_trait[ind] = 1 - diff
+        result_total[ind] = np.mean(1 - diff)
+
+    result_per_trait = np.mean(result_per_trait, axis=0)
+    print('average accuracy per trait:', result_per_trait)
+
+    result_total = np.mean(result_total)
+    print('average accuracy per video:', result_total)
+
+
+# get_accuracy('data/performance_chalearn.txt')
