d71f33bf1c7052c8d3d56697e77e4623fedcc953
redsphinx <gabi.ras@hotmail.com>
2017-12-07 16:03:23 +0100

    fixed bugs for when face is not detected

--- src/align_face/main.py
+++ src/align_face/main.py
@@ -2,16 +2,17 @@
 
 import numpy as np
 import time
+import tqdm
 from PIL import Image
 import psutil
 import skvideo.io
 import os
 from imutils.face_utils import FaceAligner
-from imutils.face_utils import rect_to_bb
+# from imutils.face_utils import rect_to_bb
 import imutils
 import dlib
 import cv2
-
+import imageio
 
 def destroy_frame():
     for proc in psutil.process_iter():
@@ -20,7 +21,7 @@ def destroy_frame():
 
 
 def show_frames(data_path, frames):
-    data_path = '/home/gabi/PycharmProjects/visualizing-traits/data/1uC-2TZqplE.003.mp4'
+    data_path = '/home/gabi/PycharmProjects/visualizing-traits/data/1Lv72Si4GnY.000./test_2.mp4'
     video_capture = skvideo.io.vread(data_path)
     video_capture = np.array(video_capture, dtype=np.uint8)
 
@@ -53,7 +54,7 @@ def align_face(image):
     for rect in rects:
         # extract the ROI of the *original* face, then align the face
         # using facial landmarks
-        (x, y, w, h) = rect_to_bb(rect)
+        # (x, y, w, h) = rect_to_bb(rect)
         # faceOrig = imutils.resize(image[y:y + h, x:x + w], width=256)
         faceAligned = fa.align(image, gray, rect)
 
@@ -76,17 +77,36 @@ def align_faces_in_video(data_path, save_location, frames=None):
         if frames is None:
             frames = np.shape(video_capture)[0]
 
-        for i in range(frames):
+        new_height, new_width, channels = 256, 256, 3
+
+        new_video_array = np.zeros((frames, new_height, new_width, channels))
+
+        no_face_counter = 0
+
+        for i in tqdm.tqdm(range(frames)):
             frame = video_capture[i]
             new_frame = align_face(frame)
-            new_image = Image.fromarray(new_frame, mode='RGB')
-            new_image_save_path = os.path.join(save_location, '%04d.jpg' % i)
-            new_image.save(new_image_save_path)
+
+            # if no face detected, copy face from previous frame
+            if new_frame is None:
+                new_frame = new_video_array[i - 1]
+                no_face_counter += 1
+                print('no face detected %d' % no_face_counter)
+
+            new_video_array[i] = new_frame
+
+            # for saving individual frames as jpg
+            # new_image = Image.fromarray(new_frame, mode='RGB')
+            # new_image_save_path = os.path.join(save_location, '%04d.jpg' % i)
+            # new_image.save(new_image_save_path)
+
+        vid_name = os.path.join(save_location, 'test_2.mp4')
+        imageio.mimwrite(vid_name, new_video_array, fps=30.)
 
     else:
         print('Error: data_path does not exist')
 
 
-data_path_ = '/home/gabi/PycharmProjects/visualizing-traits/data/1uC-2TZqplE.003.mp4'
+data_path_ = '/home/gabi/PycharmProjects/visualizing-traits/data/1Lv72Si4GnY.000.mp4'
 save_location_ = '/home/gabi/PycharmProjects/visualizing-traits/data'
-align_faces_in_video(data_path_, save_location_)
+align_faces_in_video(data_path_, save_location_, frames=None)
