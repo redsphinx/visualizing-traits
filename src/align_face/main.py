eccbcfb635ba7514d31db59c05e070d24b7d3c2f
redsphinx <gabi.ras@hotmail.com>
2017-12-08 17:22:51 +0100

    stuff

--- src/align_face/main.py
+++ src/align_face/main.py
@@ -8,45 +8,14 @@ import psutil
 import skvideo.io
 import os
 from imutils.face_utils import FaceAligner
+import util
 # from imutils.face_utils import rect_to_bb
 import imutils
 import dlib
 import cv2
 import imageio
 from multiprocessing import Pool
-
-
-def destroy_frame():
-    for proc in psutil.process_iter():
-        if proc.name() == "display":
-            proc.kill()
-
-
-def show_frames(data_path, frames):
-    data_path = '/home/gabi/PycharmProjects/visualizing-traits/data/1Lv72Si4GnY.000./test_2.mp4'
-    video_capture = skvideo.io.vread(data_path)
-    video_capture = np.array(video_capture, dtype=np.uint8)
-
-    for i in range(frames):
-        frame = Image.fromarray(video_capture[0], mode='RGB')
-        print('shape: ', np.shape(frame))
-        frame.show()
-        time.sleep(0.8)
-        destroy_frame()
-
-
-def safe_mkdir(my_path):
-    if not os.path.exists(my_path):
-        os.mkdir(my_path)
-
-
-def safe_makedirs(my_path):
-    if not os.path.exists(my_path):
-        os.makedirs(my_path)
-
-
-def get_time(t0):
-    print('time: %s' % str(time.time() - t0))
+import librosa
 
 
 def align_face(image):
@@ -95,8 +64,8 @@ def align_faces_in_video(data_path, frames=None):
 
     # tmp = '/media/gabi/DATADRIVE1/datasets/chalearn_fi_17_compressed/test-1/test80_01/3gmc2kLV4Bo.003.mp4'
 
-    which_test = data_path.strip().split('/')[6]
-    which_video_folder = data_path.strip().split('/')[7]
+    which_test = data_path.strip().split('/')[-3]
+    which_video_folder = data_path.strip().split('/')[-2]
 
     save_location = os.path.join(base_save_location, which_test, which_video_folder)
 
@@ -104,8 +73,14 @@ def align_faces_in_video(data_path, frames=None):
     #     os.makedirs(save_location)
 
     if os.path.exists(data_path):
+        # returns array with shape (459, 256, 456, 3)
         video_capture = skvideo.io.vread(data_path)
+        print(np.shape(video_capture))
         video_capture = np.array(video_capture, dtype=np.uint8)
+        # returns an array with shape (1, 1, 1, 244800)
+        audio_capture = librosa.load(data_path, 16000)[0][None, None, None, :]
+        print(np.shape(audio_capture))
+
         name_video = data_path.split('/')[-1].split('mp4')[0]
 
         if frames is None:
@@ -132,11 +107,6 @@ def align_faces_in_video(data_path, frames=None):
             new_frame = np.array(new_frame, dtype='uint8')
             new_video_array[i] = new_frame
 
-            # for saving individual frames as jpg
-            # new_image = Image.fromarray(new_frame, mode='RGB')
-            # new_image_save_path = os.path.join(save_location, '%04d.jpg' % i)
-            # new_image.save(new_image_save_path)
-
         vid_name = os.path.join(save_location, '%s.mp4' % name_video)
         # TODO: add the audio track
         imageio.mimwrite(vid_name, new_video_array, fps=30.)
@@ -145,51 +115,7 @@ def align_faces_in_video(data_path, frames=None):
         print('Error: data_path does not exist')
 
 
-def get_number_of_videos_per_folder():
-    top_folder_path = '/media/gabi/DATADRIVE1/datasets/chalearn_first_impressions_17'
-    video_suffix = 'mp4'
-
-    level_1 = os.listdir(top_folder_path)
-    print('level 0: %s' % top_folder_path.split('/')[-1])
-    for l1 in level_1:
-        l1_path = os.path.join(top_folder_path, l1)
-        if os.path.isdir(l1_path):
-            print('- level 1: %s' % l1)
-            level_2 = os.listdir(l1_path)
-            for l2 in level_2:
-                l2_path = os.path.join(l1_path, l2)
-                if os.path.isdir(l2_path):
-                    level_3 = os.listdir(l2_path)
-                    video_count = 0
-                    for l3 in level_3:
-                        if l3.strip().split('.')[-1] == video_suffix:
-                            video_count += 1
-                    print('-- level 2: %s   %d videos' % (l2, video_count))
-
-
-def align_faces_in_folder():
-    folder_path = '/media/gabi/DATADRIVE1/datasets/chalearn_fi_17_compressed'
-    save_location_top = '/media/gabi/DATADRIVE1/datasets/chalearn_fi_only_faces_aligned'
-
-    safe_mkdir(save_location_top)
-    level_1 = os.listdir(folder_path)
-    for l1 in level_1:
-        l1_path = os.path.join(folder_path, l1)
-        save_location_l1 = os.path.join(save_location_top, l1)
-        iszip = l1.strip().split('.')[-1]
-        if os.path.isdir(l1_path) and not iszip == 'zip':
-            safe_mkdir(save_location_l1)
-            level_2 = os.listdir(l1_path)
-            for l2 in level_2:
-                l2_path = os.path.join(l1_path, l2)
-                save_location_l2 = os.path.join(save_location_l1, l2)
-                if os.path.isdir(l2_path):
-                    level_3 = os.listdir(l2_path)
-                    safe_mkdir(save_location_l2)
-                    for l3 in level_3:
-                        if l3.strip().split('.')[-1] == 'mp4':
-                            l3_path = os.path.join(l2_path, l3)
-                            align_faces_in_video(l3_path, save_location_l2, frames=10)
+align_faces_in_video('/media/gabi/DATADRIVE1/datasets/chalearn_fi_17_compressed/test-1/test80_01/3gmc2kLV4Bo.003.mp4')
 
 
 def get_path_videos(name_folder):
@@ -211,15 +137,10 @@ def get_path_videos(name_folder):
     return video_path_list
 
 
-def parallel_align_1(which_folder):
-    pool = Pool(processes=40)
+def parallel_align(which_folder):
+    pool = Pool(processes=10)
     list_path_all_videos = get_path_videos(which_folder)[0:80]
     make_dirs(which_folder)
     pool.apply_async(align_faces_in_video)
     pool.map(align_faces_in_video, list_path_all_videos)
 
-
-# t = time.time()
-# parallel_align_1('test-1')
-# get_time(t)
-# make_dirs('test-2')
---
+++ src/align_face/util.py
@@ -0,0 +1,14 @@
+
+
+def safe_mkdir(my_path):
+    if not os.path.exists(my_path):
+        os.mkdir(my_path)
+
+
+def safe_makedirs(my_path):
+    if not os.path.exists(my_path):
+        os.makedirs(my_path)
+
+
+def get_time(t0):
+    print('time: %s' % str(time.time() - t0))
