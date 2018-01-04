44d68fca414c37efb35adff78d006152a7b715f0
redsphinx <gabi.ras@hotmail.com>
2018-01-04 19:07:15 +0100

    added template_from_dataset

---
+++ src/align_face/template_from_dataset.py
@@ -0,0 +1,102 @@
+import numpy as np
+import os
+from src.deepimpression import training_util as tu
+from PIL import Image
+import tqdm
+import dlib
+import cv2
+from src.align_face import util
+from src.align_face.face_utils.helpers import shape_to_np
+from scipy import ndimage
+
+
+# total of 10000 videos
+# get the face landmarks from a random frame in each video to calculate the mean position of each
+# face landmark in the training dataset
+
+# set seed for rng
+seed = 6
+
+# get all the video paths
+all_paths = []
+top_path = '/media/gabi/DATADRIVE1/datasets/chalearn_fi_17_train'
+folders_1 = os.listdir(top_path)
+for i in folders_1:
+    p1 = os.path.join(top_path, i)
+    if os.path.isdir(p1):
+        folders_2 = os.listdir(p1)
+        for j in folders_2:
+            p2 = os.path.join(p1, j)
+            if os.path.isdir(p2):
+                videos = os.listdir(p2)
+                for v in videos:
+                    p3 = os.path.join(p2, v)
+                    all_paths.append(p3)
+
+del folders_1
+
+# for all videos, get random frame, get the landmark and store in array
+height = 360
+width = 640
+channels = 3
+number_landmarks = 68
+# num_videos = len(all_paths)
+num_videos = 3
+random_frame_landmarks_from_all_videos = np.zeros((num_videos, number_landmarks * 2))
+predictor = '/home/gabi/PycharmProjects/visualizing-traits/data/predictor/shape_predictor_68_face_landmarks.dat'
+predictor = dlib.shape_predictor(predictor)
+detector = dlib.get_frontal_face_detector()
+
+
+for i in tqdm.tqdm(range(num_videos)):
+    retries = 0
+    tmp_seed = seed
+    p = all_paths[i]
+    largest_face_rectangle = None
+    gray = None
+    while largest_face_rectangle is None and retries < 5:
+        arr_frame = tu.get_random_frame(p, tmp_seed)
+        image_frame = Image.fromarray(arr_frame, mode='RGB')
+        image_frame = image_frame.resize((width, height), Image.ANTIALIAS)
+        arr_frame = list(image_frame.getdata())
+        arr_frame = np.reshape(arr_frame, (height, width, channels)).astype(np.uint8)
+        gray = cv2.cvtColor(arr_frame, cv2.COLOR_BGR2GRAY)
+        face_rectangles = detector(gray, 2)
+        largest_face_rectangle = util.find_largest_face(face_rectangles)
+        if largest_face_rectangle is None:
+            tmp_seed += 1
+            retries += 1
+            print(retries)
+
+    try:
+        landmarks = shape_to_np(predictor(gray, largest_face_rectangle))
+    except TypeError:
+        arr_frame = ndimage.imread('backup_face.jpg')
+        face_rectangles = detector(arr_frame, 2)
+        largest_face_rectangle = util.find_largest_face(face_rectangles)
+        landmarks = shape_to_np(predictor(arr_frame, largest_face_rectangle))
+
+    landmarks = np.ndarray.reshape(landmarks, (number_landmarks * 2))
+    random_frame_landmarks_from_all_videos[i] = landmarks
+
+# get the mean landmarks
+mean_landmark = np.mean(random_frame_landmarks_from_all_videos, axis=0).astype(int)
+mean_landmark = np.reshape(mean_landmark, (68, 2))
+
+# draw the landmarks
+canvas = np.ones((height, width, 3)).astype(np.uint8)
+canvas *= 255
+print(mean_landmark)
+for p in mean_landmark:
+    x, y = p
+    canvas[y, x] = [0, 0, 0]
+
+img = Image.fromarray(canvas, mode='RGB')
+img.save('TEMPLATE_%d.jpg' % num_videos)
+
+# write landmark to file
+file_path = '/home/gabi/PycharmProjects/visualizing-traits/src/align_face/landmark_template.txt'
+with open(file_path, 'w') as my_file:
+    for i in range(number_landmarks):
+        x, y = mean_landmark[i]
+        my_file.write('%d,%d\n' % (x, y))
