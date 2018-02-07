7e226563171a6f0bcb7219c9191cb45295bf7997
redsphinx <gabi.ras@hotmail.com>
2018-02-07 14:52:20 +0100

    aligning testing data added

--- src/align_face/project_paths.py
+++ src/align_face/project_paths.py
@@ -2,13 +2,19 @@ VAR_LOCAL = True
 
 if VAR_LOCAL:
     PREDICTOR = '/home/gabi/PycharmProjects/visualizing-traits/data/predictor/shape_predictor_68_face_landmarks.dat'
-    DATA_PATH = '/media/gabi/DATADRIVE1/datasets/chalearn_fi_17_train'
-    BASE_SAVE_LOCATION = '/home/gabi/PycharmProjects/visualizing-traits/data/training'
+    # for train data
+    # DATA_PATH = '/media/gabi/DATADRIVE1/datasets/chalearn_fi_17_train'
+    # BASE_SAVE_LOCATION = '/home/gabi/PycharmProjects/visualizing-traits/data/training'
+    # for test data
+    DATA_PATH = '/media/gabi/DATADRIVE1/datasets/chalearn_first_impressions_17'
+    BASE_SAVE_LOCATION = '/media/gabi/DATADRIVE1/datasets/chalearn_test_aligned'
     TEMPLATE = '/home/gabi/PycharmProjects/visualizing-traits/src/align_face/cropped_landmark_template.txt'
 else:
     # PREDICTOR = '/home/gabras/visualizing-traits/data/predictor/shape_predictor_68_face_landmarks.dat'
     PREDICTOR = '/vol/ccnlab-scratch1/gabras/predictor-landmarks/shape_predictor_68_face_landmarks.dat'
-    DATA_PATH = '/vol/ccnlab-scratch1/gabras/chalearn_train/'
-    BASE_SAVE_LOCATION = '/vol/ccnlab-scratch1/gabras/chalearn_train_aligned'
+    # DATA_PATH = '/vol/ccnlab-scratch1/gabras/chalearn_train/'
+    # BASE_SAVE_LOCATION = '/vol/ccnlab-scratch1/gabras/chalearn_train_aligned'
+    DATA_PATH = '/vol/ccnlab-scratch1/gabras/chalearn_compressed/'
+    BASE_SAVE_LOCATION = '/vol/ccnlab-scratch1/gabras/chalearn_test_aligned'
     TEMPLATE = '/vol/ccnlab-scratch1/gabras/predictor-landmarks/cropped_landmark_template.txt'
     # TEMPLATE = '/home/gabras/visualizing-traits/src/align_face/cropped_landmark_template.txt'
